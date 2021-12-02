f7bfb82d8f217446ad3e67bd8b0d486643d026d1
Deekshith M D <70252174+deekshithmd@users.noreply.github.com>
2021-12-02 11:16:22 +0530

    update: useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import React, { useState } from "react";
 import "./styles.css";
-var name = prompt("Enter your name");
-var color = "blue";
-
+//var name = prompt("Enter your name");
+//Welcome <span style={{ color: color }}>{name}</span>
+//var color = "blue";
+//var likeCounter = 0;
 export default function App() {
+  const [likeCounter, setlikeCounter] = useState(0);
+  function likeclickHandler() {
+    var newlikeCounter = likeCounter + 1; //processing
+    console.log("clicked " + likeCounter + " times");
+    setlikeCounter(newlikeCounter);
+  }
   return (
     <div className="App">
-      <h1>
-        Welcome <span style={{ color: color }}>{name}</span>
-      </h1>
+      <h1> Inside outt</h1>
+      <button onClick={likeclickHandler}>click</button>
+      {likeCounter}
     </div>
   );
 }
